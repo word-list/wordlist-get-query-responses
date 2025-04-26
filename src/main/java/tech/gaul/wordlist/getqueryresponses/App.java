3ea07856360557279cc64e1c9e6e285c305d08a5
Jonathan Gaul <jon@gaul.tech>
2025-04-26 23:10:39 +0000

    Initial commit

---
+++ src/main/java/tech/gaul/wordlist/getqueryresponses/App.java
@@ -0,0 +1,12 @@
+package tech.gaul.wordlist.getqueryresponses;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class App implements RequestHandler<Object, Object> {
+    @Override
+    public Object handleRequest(Object event, Context context) {
+        // This will be a time triggered event        
+        return null;
+    }
+}
